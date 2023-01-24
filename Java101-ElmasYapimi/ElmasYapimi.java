d752a8a708f517460aaffce9e8dbc19287116062
Mert Cem <mertcemavinca018@gmail.com>
2023-01-24 18:30:15 +0300

    Add files via upload

---
+++ Java101-ElmasYapimi/ElmasYapimi.java
@@ -0,0 +1,29 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class ElmasYapimi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("bir sayı gir : ");
+        int sayi = inp.nextInt();
+        for (int i=1;i<=sayi;i++){
+            for (int k = 1 ; k <=(sayi-i);k++){
+                System.out.print(" ");
+            }
+            for (int k =1 ; k<=(2*i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i =sayi-1 ; i>=1;i--){
+            for (int k =(sayi-i);k>=1;k--){
+                System.out.print(" ");
+            }
+            for (int k =1 ; k<=(2*i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
