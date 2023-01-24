5c53fff751b953ee6034bea1bf7d4043c08c4139
Mert Cem <mertcemavinca018@gmail.com>
2023-01-24 14:53:57 +0300

    Update UsluSayiHesap.java

--- Java101-UsluSayiHesap/UsluSayiHesap.java
+++ Java101-UsluSayiHesap/UsluSayiHesap.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class UsluSayiHesap {
     public static void main(String[] args) {
-        int a,b,sonuc=0;
+        int a,b,sonuc=1;
         Scanner inp = new Scanner(System.in);
         System.out.print("taban giriniz : ");
         a = inp.nextInt();
         System.out.print("üs giriniz : ");
         b = inp.nextInt();
         for (int i = 1 ; i < b ; i++ ){
-            sonuc += a*a;
+            sonuc *=a;
         }
         System.out.println("sonuç = " +sonuc);
 
