718334ab747a2d97703c3150f2e7d90031006bb0
Mert Cem <mertcemavinca018@gmail.com>
2023-01-25 12:45:18 +0300

    Add files via upload

---
+++ Java101-FibonacciSerisi/FibonacciSerisi.java
@@ -0,0 +1,21 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class FibonacciSerisi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int eleman,k=0,l=1;
+        System.out.print("eleman sayısı girin : ");
+        eleman = inp.nextInt();
+        for (int i = 1; i<=eleman;i++){
+            //toplam+l=toplam
+            //toplam+l=toplam
+            //l+toplam=toplam
+            System.out.println(k);
+            k=k+l;
+            l=k-l;
+
+        }
+    }
+}
