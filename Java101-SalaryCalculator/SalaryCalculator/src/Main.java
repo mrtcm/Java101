3988758961f4b6f113ab97fc581b48c92db727ae
Mert Cem <mertcemavinca018@gmail.com>
2023-02-08 15:09:16 +0300

    Add files via upload

---
+++ Java101-SalaryCalculator/SalaryCalculator/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("mert cem",900,39,2021);
+        employee1.tooString();
+    }
+}
