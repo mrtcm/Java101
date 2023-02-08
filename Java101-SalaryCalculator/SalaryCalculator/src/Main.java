b3eb8d0450806dbf85f93467bf3828035f4150f3
Mert Cem <mertcemavinca018@gmail.com>
2023-02-08 15:08:18 +0300

    Delete Java101-SalaryCalculator directory

--- Java101-SalaryCalculator/SalaryCalculator/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Employee employee1 = new Employee("mert cem",900,39,2021);
-        employee1.tooString();
-    }
-}
