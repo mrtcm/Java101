2e9a71d4e867a0c7f3c05ee517f4f980ae367b46
Mert Cem <mertcemavinca018@gmail.com>
2023-02-25 20:33:44 +0300

    Add files via upload

---
+++ Java101-DizidekiTekrarEdenSayilari/Readme.md
@@ -0,0 +1,7 @@
+# Java101-DizidekiTekrarEdenSayilari
+
+Ödev
+Şimdi sıra sende! Bir sayı dizisindeki tekrar eden çift sayıları belirten bir program yazarak GitHub reposunu bizimle paylaşmalısın. Kolay gelsin.
+
+
+www.patika.dev
---
+++ Java101-DizidekiTekrarEdenSayilari/TekrarEdenSayi.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class TekrarEdenSayi {
+    static boolean kontrol(int[] dizi ,  int value ){
+        for(int i : dizi){
+            if (value ==  i ){
+                return false;
+            }
+        }
+        return  true;
+    }
+    public static void main(String[] args) {
+        int[] dizi  = {1,2,123,1,23,2,32,123,1};
+        int[] tkr  = new int[dizi.length];
+        int sayac  = 0;
+        for (int i = 0 ; i< dizi.length;i++){
+            for(int j = 0 ; j < dizi.length;j++){
+                if (i != j && dizi[i]== dizi[j]){
+                    if(kontrol(tkr,dizi[i]) && (dizi[i]%2 == 0)){
+                        tkr[sayac++] = dizi[i];
+                    }
+                    break;
+                }
+
+            }
+        }
+        System.out.println(Arrays.toString(tkr));
+    }
+}
