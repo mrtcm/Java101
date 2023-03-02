20240d2b5760bfab37bb0af0c6138779eee47fc3
Mert Cem <mertcemavinca018@gmail.com>
2023-03-02 22:55:20 +0300

    Add files via upload

---
+++ Java101-DizidekiElemanFrekansi/TkrSayi.java
@@ -0,0 +1,14 @@
+public class TkrSayi {
+    public static void main(String[] args) {
+        int[] sayi ={12,1,212,2,12,11,1,1,212,0};
+        for (int i = 0 ; i<sayi.length; i++){
+            int sayac = 0;
+            for (int j : sayi){
+                if (sayi[i] == j){
+                    sayac++;
+                }
+            }
+            System.out.println(sayi[i] + " sayısı dizide " + sayac + " kadar tekrar ediyor ");
+        }
+    }
+}
